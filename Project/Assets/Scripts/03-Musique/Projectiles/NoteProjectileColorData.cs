cfcb18eacd1a597eaf5bdc01399997c3db42cea6
Mathilde-Minaud <minaud.mathilde@gmail.com>
2024-01-05 18:34:24 +0100

    ajout des prefab d'everhood

---
+++ Project/Assets/Scripts/03-Musique/Projectiles/NoteProjectileColorData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ProjectileColor
+{
+    Fire, Blue, Red, Green, Psy, Drought,Purple,Black
+}
+public class NoteProjectileColorData : MonoBehaviour
+{
+    public ProjectileColor projectileColor;
+}
